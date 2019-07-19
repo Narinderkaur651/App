cd298d76b07db7caa8897512f5dadbea3a5475d8
Narinderkaur651 <49247092+Narinderkaur651@users.noreply.github.com>
2019-07-19 10:58:08 +0530

    background update

--- jquery.js
+++ jquery.js
@@ -1,146 +1,51 @@
-let dataHtml = "";
+function getData(url, successCallback) {
+  $.ajax({
+      url: url,
+      success: successCallback
+  });
+}
 
-let personData = [{
-    "userId": 21,
-    "id": 3,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 14,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-  },
-  {
-    "userId": 7,
-    "id": 4,
-    "title": "eum et est occaecati",
-    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-  },
-  {
-    "userId": 18,
-    "id": 5,
-    "title": "nesciunt quas odio",
-    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-  },
-  {
-    "userId": 41,
-    "id": 6,
-    "title": "dolorem eum magni eos aperiam quia",
-    "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
-  },
-  {
-    "userId": 3,
-    "id": 7,
-    "title": "magnam facilis autem",
-    "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
-  },
-  {
-    "userId": 1,
-    "id": 8,
-    "title": "dolorem dolore est ipsam",
-    "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "nesciunt iure omnis dolorem tempora et accusantium",
-    "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
-  },
-  {
-    "userId": 1,
-    "id": 10,
-    "title": "optio molestias id quia eum",
-    "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
-  },
-  {
-    "userId": 2,
-    "id": 11,
-    "title": "et ea vero quia laudantium autem",
-    "body": "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
-  },
-  {
-    "userId": 2,
-    "id": 12,
-    "title": "in quibusdam tempore odit est dolorem",
-    "body": "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
-  },
-  {
-    "userId": 2,
-    "id": 13,
-    "title": "dolorum ut in voluptas mollitia et saepe quo animi",
-    "body": "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
-  },
-  {
-    "userId": 2,
-    "id": 14,
-    "title": "voluptatem eligendi optio",
-    "body": "fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum"
-  },
-  {
-    "userId": 2,
-    "id": 15,
-    "title": "eveniet quod temporibus",
-    "body": "reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae"
-  },
+$(document).ready(function () {
+  getData("https://jsonplaceholder.typicode.com/posts", function (response) {
+      $(response).each(function (i, response) {
+          var datahtml = `<tr><td>${response.userId}</td><td>${response.id}</td><td>${response.title}</td><td>${response.body}</td><td><button type="button" class="btn btn-sm dlete" >remove</button></td></tr>`;
+          $("tbody").append(datahtml);
+      });
+  });
 
-];
-/*--add dynamic data---*/
-window.onload = function () {
-  function loadTableData(personData) {
-    const tableBody = document.getElementById('tableData');
-    console.log("tableBody");
-    console.log(tableBody);
-    for (let person of personData) {
-      dataHtml += `<tr><td>${person.userId}</td><td>${person.id}</td><td>${person.title}</td><td>${person.body}</td><td><button type="button" class="btn btn-sm dlete" >remove</button></td></tr>`;
-    };
-    tableBody.innerHTML = dataHtml;
-  }
-  loadTableData(personData);
-  /*--add user data--*/
-  var userData = $("#submit-data").click(function (e) {
-    var _userId = $('input[name="uid"]').val();
-    var _date = $('input[name="date"]').val();
-    var _title = $('input[name="title"]').val();
-    var _body = $('input[name="body"]').val();
+  /*-- user-input data--*/
+  $("#submit-data").click(function (e) {
+      var userId1 = $('input[name="uid"]').val();
+      var date1 = $('input[name="date"]').val();
+      var title1 = $('input[name="title"]').val();
+      var body1 = $('input[name="body"]').val();
 
-    const _dataHtml1 = '<tr><td>' + _userId + '</td><td>' + _date + '</td><td>' + _title + '</td><td>' + _body + '</td><td><button type="button" class="btn btn-sm dlete" >remove</button></td></tr>';
+      const dataHtml1 = '<tr><td>' + userId1 + '</td><td>' + date1 + '</td><td>' + title1 + '</td><td>' + body1 + '</td><td><button type="button" class="btn btn-sm dlete" >remove</button></td></tr>';
 
-    $('tbody').append(_dataHtml1);
-    var _userId = $('input[name="uid"]').val("");
-    var _date = $('input[name="date"]').val("");
-    var _title = $('input[name="title"]').val("");
-    var _body = $('input[name="body"]').val("");
+      if (userId1 !== "" || date1 !== "" || title1 !== "" || body1 !== "") {
+          $('tbody').append(dataHtml1);
+      }
 
-    e.preventDefault();
+      var userId1 = $('input[name="uid"]').val("");
+      var date1 = $('input[name="date"]').val("");
+      var title1 = $('input[name="title"]').val("");
+      var body1 = $('input[name="body"]').val("");
+      e.preventDefault();
   });
   $(document).on('click', '.dlete', function () {
-    $(this).closest('tr').remove();
+      $(this).closest('tr').remove();
   })
-}
-/*--sort--*/
-personData.sort(function (a, b) {
-  return a.userId - b.userId;
-});
-/*--serach--*/
-$(document).ready(function () {
   $("#search-data").on("keyup", function () {
-    var value = $(this).val().toLowerCase();
-    $("#tableData tr").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
+      var value = $(this).val().toLowerCase();
+      $("#tableData tr").filter(function () {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  });
+  $("form").hide(function () {
+      $("#add-data").click(function () {
+          $("form").toggle();
+      });
   });
 });
-/*--form-toggle--*/
-$(document).ready(function () {
-  $("#add-data").click(function () {
-    $("form").toggle();
-  });
-});
+
+
