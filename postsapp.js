3d7a40e85ec2a2a65b7df1f59626c6dfd5f61191
Narinderkaur651 <49247092+Narinderkaur651@users.noreply.github.com>
2019-08-20 19:09:51 +0530

    accordion update

--- postsapp.js
+++ postsapp.js
@@ -1,52 +1,220 @@
-/**
- * 
- * @param {string} url API url
- * @param {callback} successCallback Success callback for function
- */
-function getData(url, successCallback) {
-  $.ajax({
-    url: url,
-    success: successCallback
-  });
-}
-
 $(document).ready(function () {
-  getData("https://jsonplaceholder.typicode.com/posts", function (response) {
-    $(response).each(function (i, response) {
-      let datahtml = `<tr><td>${response.userId}</td><td>${response.id}</td><td>${response.title}</td><td>${response.body}</td><td><button type="button" class="btn btn-sm dlete" >Remove</button></td></tr>`;
-      $("tbody").append(datahtml);
-    });
-  });
-  /*-- user-input data--*/
-  $("#submit-data").click(function (e) {
-    e.preventDefault();
-    let personid = $('input[name="uid"]').val();
-    let date = $('input[name="date"]').val();
-    let title = $('input[name="title"]').val();
-    let body = $('input[name="body"]').val();
-    const Datahtml = '<tr><td>' + personid + '</td><td>' + date + '</td><td>' + title + '</td><td>' + body + '</td><td><button type="button" class="btn btn-sm dlete" >Remove</button></td></tr>';
-    if (personid !== "" || date !== "" || title !== "" || body !== "") {
-      $('tbody').append(Datahtml);
+    let personrecord = [{
+        "UserId": 1,
+        "UserName": "shiwali",
+        "PostTitle": "reprehenderit dolor",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 2,
+        "UserName": "raman",
+        "PostTitle": "sint nihil reprehenderit",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 3,
+        "UserName": "renu",
+        "PostTitle": "beatae ea dolores neque",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 4,
+        "UserName": "aamar",
+        "PostTitle": "dolor beatae ea dolores",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 5,
+        "UserName": "shiwam_mehta",
+        "PostTitle": "dolor beatae ea",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 6,
+        "UserName": "rahul",
+        "PostTitle": "reprehenderit dolor beatae",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 6,
+        "UserName": "rvina",
+        "PostTitle": "gdjj djd kkkd",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 7,
+        "UserName": "baljet kaur",
+        "PostTitle": "dolor beatae ea",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 8,
+        "UserName": "menu",
+        "PostTitle": "sint nihil reprehenderit",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 9,
+        "UserName": "ritu",
+        "PostTitle": "gdjj djd kkkd",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
+    }, {
+        "UserId": 10,
+        "UserName": "shiwali",
+        "PostTitle": "gdjj djd kkkd",
+        "PostMessage": " tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro"
     }
-    $('input[name="uid"]').val("");
-    $('input[name="date"]').val("");
-    $('input[name="title"]').val("");
-    $('input[name="body"]').val("");
-  });
-  $(document).on('click', '.dlete', function () {
-    $(this).closest('tr').remove();
-  })
-  $("#search-data").on("keyup", function () {
-    let value = $(this).val().toLowerCase();
-    $("#tableData tr").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    ]
+    $(personrecord).each(function (i, personrecord) {
+        let recordhtml =
+            `<div class="data-remove post-content">
+            <button class="accordion pl-4 pr-4"> 
+        <div class="row row-data  ">
+        <div class="col-md-3 post-content">
+        ${personrecord.UserId}
+        </div>
+        <div class="col-md-3 post-content">
+       <img src="https://picsum.photos/id/846/40/40" class="rounded-circle mr-2 img-fluid"> ${personrecord.UserName}
+        </div>
+        <div class="col-md-3 post-content">
+        ${personrecord.PostTitle}
+        </div>
+       
+     <div class="col-md-3 post-content">
+    Status
+        </div>
+
+       
+       </div>
+        </button> 
+        <div class="panel">
+        <div class="row row-data ">
+            
+           
+            <div class="col-md-9">
+             ${personrecord.PostMessage}
+            </div> 
+           <div class="col-md-3">  <button type="button" class="btn btn-outline-info dlete pt-1 pb-1"  >Remove</button></div>
+        </div></div></div>
+    `;
+
+        console.log(personrecord);
+        $(".table-data").append(recordhtml);
     });
-  });
-  $("form").hide(function () {
-    $("#add-data").click(function () {
-      $("form").toggle();
+    console.log(personrecord);
+    /*--acordion--*/
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+
+    // var accordion = document.getElementsByClassName("accordion");
+    // var i;
+    // for (var i = 0; i < accordion.lenght; i++) {
+    //     accordion[i].onclick = function () {
+    //         var content = this.nextElementSibling;
+    //         if (content.style.maxHeight) {
+    //             content.style.maxHeight = null;
+    //         }
+    //         else {
+    //             content.style.maxHeight = content.scrollHeight + "px";
+    //         }
+    //     }
+    // }
+    /*--user-data--*/
+    $("#submit-data").click(function (e) {
+        e.preventDefault();
+        let date = $('input[name="date"]').val();
+        let title = $('input[name="title"]').val();
+        let body = $('input[name="body"]').val();
+
+        gnfn = (function () {
+            var random = Math.floor(Math.random() * 100 + 1);
+            return random;
+        })();
+
+
+        const Datahtml = `<div class="data-remove">
+        <button class="accordion pl-4 pr-4"> 
+        <div class="row row-data ">
+        <div class="col-md-3">
+        ${gnfn}
+        </div>
+        <div class="col-md-3">
+        <img src="https://picsum.photos/id/846/40/40" class="rounded-circle img-fluid"> ${date}
+        </div>
+        <div class="col-md-3">
+        ${title}
+        </div>   
+         
+        <div class="col-md-3"> Status </div>
+       </div>
+        </button>
+        <div class="panel">
+        <div class="row row-data">       
+            <div class="col-md-8">
+             ${body}
+            </div> 
+           <div class="col-md-4"> 
+           <button type="button" class="btn btn-outline-info dlete">Remove</button>
+           </div> 
+        </div></div></div>`;
+        console.log(Datahtml);
+        /*----*/
+        if (date !== "" || title !== "" || body !== "") {
+            $('.table-data').append(Datahtml);
+        }
+        $('input[name="date"]').val("");
+        $('input[name="title"]').val("");
+        $('input[name="body"]').val("");
     });
-  });
+    /*--remove--*/
+    $(document).on('click', '.dlete', function () {
+        $(this).closest('.data-remove').remove();
+    });
+
+    /*--add-data--*/
+    $("form").hide(function () {
+        $("#add-data").click(function () {
+            $("form").toggle();
+        });
+    });
+    /*--search--*/
+    $("#search-data").on("keyup", function () {
+        console.log("keyup");
+        let value = $(this).val().toLowerCase();
+        $(".table-data .accordion").filter(function () {
+            const searchStatus = $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+            if (!searchStatus) {
+                $('#error').find("#error").show(); $(".hide").remove().css("display", "block");
+            } else {
+                $('#error').find("#error").hide();
+
+            }
+        });
+        // $('#search-data').on('keyup', function () {
+        //     var isResultFound = false;
+        //     var searchTerm = $(this).val().toLowerCase();
+        //     $('.post-content').each(function () {
+        //         if ($(this).filter(searchTerm).length > 0) {
+        //             isResultFound = true;
+        //             $(this).show();
+        //         } else {
+        //             $(this).hide();
+        //         }
+
+        //         if (!isResultFound) {
+        //             $('#error').show(); $(".hide").remove();
+        //         } else {
+        //             $('#error').hide();
+
+        //         }
+        //     });
+        // });
+        /*--pagination--*/
+
+    });
 });
 
 
